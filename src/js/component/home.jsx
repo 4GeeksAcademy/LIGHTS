5a30a42c4f53ec7fbf9ef9937f23d19d64832e11
kjatkinson <149319433+kjatkinson@users.noreply.github.com>
2024-03-07 00:25:21 +0000

    LIGHTS

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -10,10 +10,13 @@ export function Home() {
 	return (
 		<div className="trafficLight">
 			<div 
-				className={"light red" + (selectedColor=== "red" ? "glow" : "")}></div>
+			onClick={()=> setSelectedColor("red")}
+				className={"light red" + (selectedColor=== "red" ? " glow" : "")}></div>
 			<div 
+				onClick={() => setSelectedColor("yellow")}
 				className={"light yellow" + (selectedColor=== "yellow" ? "glow" : "")}></div>
 				<div 
+				onClick={() => setSelectedColor ("green")}
 				className={"light green" + (selectedColor=== "green" ? "glow" : "")}></div>
 			</div>
 
